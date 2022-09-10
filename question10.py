dd057c7ac9c0e7131ef2ea730d4eac9df42d08ae
ANKUR TYAGI <52342975+ankurtyagimmit@users.noreply.github.com>
2022-09-11 01:17:58 +0530

    Assignment1
    This is a python Assignment

---
+++ question10.py
@@ -0,0 +1,6 @@
+print(" These Values are integer:")
+a=5
+b=20
+c=80
+d=63
+print(" Value of a is :",a,"\n","Value of b is :", b,"\n","Value of c is :",c,"\n","Value of d is :",d,)
---
+++ question9.py
@@ -0,0 +1,6 @@
+a="Ankur Tyagi"
+b="27 years old"
+c="BCA II Year"
+d="c and C++ 6 month Experience"
+print(" Name :",a,"\n","Age :",b,"\n","Qualification :",c,"\n","Experience :",d,)
+
